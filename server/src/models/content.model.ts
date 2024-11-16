a2269302a4cb606b701f17948c389a8d324a517e
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-17 02:30:10 +0530

    content modle done

---
+++ server/src/models/content.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Types } from "mongoose"
+import { Schema, model } from "mongoose"
+
+interface IContentTypes {
+    link: string;
+    type: ["image", "video", "article", "audio"];
+    title: string;
+    tags: Types.ObjectId[];
+    userId: Types.ObjectId;
+}
+
+const contentSchema = new Schema<IContentTypes>({
+    link: {
+        type: String,
+        required: true
+    },
+    type: {
+        enum: ["image", "video", "article", "audio"],
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    tags: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "tag"
+    }],
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    }
+}, { timestamps: true })
+
+const Content = model<IContentTypes>("content", contentSchema)
+
+export default Content
