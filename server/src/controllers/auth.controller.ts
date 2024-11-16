2947a776e08d96ca81ff4bfc4c078d7ae936c014
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-17 02:52:20 +0530

    auth controllers

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+import User from "../models/user.model"
+
+export const handleSignUp = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body
+
+        const user = await User.find({ username })
+        if (user) {
+            res.status(403).json({ message: "User already exists with this username" })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
+            username,
+            hashedPassword
+        })
+
+        await newUser.save()
+
+        res.status(200).json({ message: "Signed Up" })
+    } catch (error) {
+        res.status(500).json({ message: "Server error" })
+    }
+}
+
+export const handleLogin = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body
+
+        const user = await User.findOne({ username })
+        if (!user) {
+            res.status(403).json({ message: "User doesnt exist" })
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user!.password)
+        if (!passwordMatch) {
+            res.status(403).json({ message: "Wrong password" })
+        }
+
+        const secretKey = process.env.SECRET_KEY as string
+        const token = jwt.sign({ userId: user?._id }, secretKey, { expiresIn: '24hr' })
+
+        res.send(token)
+    } catch (error) {
+
+    }
+}
