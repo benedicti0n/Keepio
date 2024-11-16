8e34a19e984bc9841e047fd10aed7fddeb82dbbe
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-17 02:52:46 +0530

    authRoutes added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
+import "dotenv/config"
 import express, { Request, Response } from "express"
 
+import authRoutes from "./routes/auth.routes"
+
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT
 
 app.get("/", (req: Request, res: Response) => {
     res.send("hello world")
 })
+
+app.use("api/v1", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
