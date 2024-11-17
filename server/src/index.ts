f24493fe9e117078a3c159fed0c909669063e9a7
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-17 08:17:13 +0530

    cors config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import express from "express"
+import cors from "cors"
 
 import authRoutes from "./routes/auth.routes"
 import contentRoutes from "./routes/content.routes"
@@ -7,9 +8,16 @@ import connectDB from "./utils/connect"
 
 const app = express()
 const PORT = process.env.PORT
+const clientUrl = process.env.CLIENT_URL
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 connectDB()
 
