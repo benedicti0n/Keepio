a569ea92a673a228593baf051e5efa265ce82c99
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-17 08:18:36 +0530

    auth routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { handleSignUp, handleLogin } from "../controllers/auth.controllers"
+import { handleSignUp, handleLogin } from "../controllers/auth.controller"
 
 const router = express.Router()
 
